2e71155ed7097bc142b7295977809f14e72bcfbf
viniciusgrangeiro <vinicius,grangeiro@gmai.com>
2024-12-13 18:09:51 -0300

    criei funcoes para cadastrar e listar os restaurante e refatorei trechos que se repetiam, como exibir subtitulo e voltar ao menu

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
+
 def exibir_nome_programa():
     print('''
     █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
@@ -12,29 +14,50 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
+def voltar_menu_principal():
+    input('\nPrecione ENTER para voltar ao menu principal ')
+    main()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(f'{texto}\n')
+
 def escolher_opcoes():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         match opcao_escolhida:
             case 1:
-                print('Cadastrar restaurante\n')
+                cadastrar_restaurante()
             case 2:
-                print('Listar restaurante\n')
+                listar_restaurantes()
             case 3:
-                print('Ativar restaurante\n')
+                ativar_restaurante()
             case 4: # 
                 finalizar_app()
     except:
         opcao_invalida()
 
-def finalizar_app() :
-    os.system('cls') # usado para limpar o terminal
-    print('Finalizando o app\n')
+def cadastrar_restaurante():
+    exibir_subtitulo('Cadastrar novo restaurante')
+    nome_restaurante = input('Informe o nome do restaurante: ')
+    print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso\n')
+    restaurantes.append(nome_restaurante)
+    voltar_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes cadastrados')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_menu_principal()
 
 def opcao_invalida():
     print('Opção inválida')
-    input('Aperte alguma tecla para retornar ao menu principal\n')
-    main()
+    voltar_menu_principal()
+
+def finalizar_app() :
+    exibir_subtitulo('FInalizando app')
 
 
 def main():
