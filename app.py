2985758d8e42d5878ac2b109d9f0e141b42849fd
viniciusgrangeiro <vinicius,grangeiro@gmai.com>
2024-12-15 21:32:34 -0300

    Criei um dicionario para ser armazenados os restaurantes e criei a funcao para alterar o status de ativo do restaurante desejado

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 
-restaurantes = ['Pizza', 'Sushi']
+# usando o 'Dicionario', podemos adicionar varias informacoes com chave e valor a um unico indice
+restaurantes = [{'nome' : 'Praça', 'categoria' : 'Japonesa', 'ativo' : False},
+                {'nome' : 'Pizza Suprema', 'categoria' : 'Italiana', 'ativo' : True},
+                {'nome' : 'Cantina', 'categoria' : 'Italiana', 'ativo' : False}]
 
 def exibir_nome_programa():
     print('''
@@ -11,7 +14,7 @@ def exibir_nome_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar status do restaurante')
     print('4. Sair\n')
 
 def voltar_menu_principal():
@@ -23,32 +26,70 @@ def exibir_subtitulo(texto):
     print(f'{texto}\n')
 
 def escolher_opcoes():
-    try:
-        opcao_escolhida = int(input('Escolha uma opção: '))
-        match opcao_escolhida:
-            case 1:
-                cadastrar_restaurante()
-            case 2:
-                listar_restaurantes()
-            case 3:
-                ativar_restaurante()
-            case 4: # 
-                finalizar_app()
-    except:
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    if opcao_escolhida == 1:
+        cadastrar_restaurante()
+    elif opcao_escolhida == 2:
+        listar_restaurantes()
+    elif opcao_escolhida == 3:
+        alternar_status_restaurante()
+    elif opcao_escolhida == 4:
+        finalizar_app()
+    else:
         opcao_invalida()
+
+
+    # try:
+    #     opcao_escolhida = int(input('Escolha uma opção: '))
+    #     match opcao_escolhida:
+    #         case 1:
+    #             cadastrar_restaurante()
+    #         case 2:
+    #             listar_restaurantes()
+    #         case 3:
+    #             print('Ativar Restaurante')
+    #             #ativar_restaurante()
+    #         case 4: # 
+    #             finalizar_app()
+    # except:
+    #     opcao_invalida()
 
 def cadastrar_restaurante():
     exibir_subtitulo('Cadastrar novo restaurante')
     nome_restaurante = input('Informe o nome do restaurante: ')
+    categoria_restaurante = input(f'Informe a categoria do restaurante: ')
+    novo_restaurante = {'nome' : nome_restaurante, 'categoria' : categoria_restaurante, 'ativo' : False}
     print(f'O restaurante {nome_restaurante} foi cadastrado com sucesso\n')
-    restaurantes.append(nome_restaurante)
+    restaurantes.append(novo_restaurante)
     voltar_menu_principal()
 
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes cadastrados')
 
     for restaurante in restaurantes:
-        print(f'.{restaurante}')
+        nome_restaurente = restaurante['nome']
+        categoria_restaurante = restaurante['categoria']
+        status_restaurante = restaurante['ativo']
+        print(f'- {nome_restaurente} | {categoria_restaurante} | {status_restaurante}') 
+
+    voltar_menu_principal()
+
+def alternar_status_restaurante():
+    exibir_subtitulo('Alternar status de restaurante')
+
+    nome = input('Informe o nome do restaurante: ')
+    encontrado = False
+    
+    for restaurante in restaurantes:
+        if restaurante['nome'] == nome:
+            encontrado = True
+            restaurante['ativo'] = not restaurante['ativo'] # Inverteu o valor que esta na chave de ativo, seja ele False ou True
+            # Ternário
+            mensagem = f'O Restaurante {nome} agora está ATIVO' if restaurante['ativo']  else f'O restaurante {nome} agora está DESATIVADO'
+            print(mensagem)
+    
+    if not encontrado:
+        print(f'O restaurante {nome} não foi encontrado')
 
     voltar_menu_principal()
 
